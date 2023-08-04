b84d56ea0c48cef9a358e309f2109dc2f09029ae
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-08-04 14:12:22 +0530

    main file created

---
+++ MiniProjects/StudentManagementSystem/Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println('Walcome to Student management system.');
+        
+    }
+}

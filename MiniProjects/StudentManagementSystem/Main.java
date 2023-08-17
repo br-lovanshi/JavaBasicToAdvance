a89ca885e11b005cf7246ce0e16f541eb42abbc0
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-08-17 23:15:10 +0530

    update

--- MiniProjects/StudentManagementSystem/Main.java
+++ MiniProjects/StudentManagementSystem/Main.java
@@ -2,7 +2,10 @@ class Main{
     public static void main(String[] args){
         
         Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        String str = sc.nextLine();
         System.out.println('Walcome to Student management system.');
+        system.out.println(str);
         
     }
 }

1463e11b76eddccc917fd5ec81248e16b33d6e1e
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-08-17 23:18:03 +0530

    spring boot main class

---
+++ SpringBootexceptionhandling/src/main/java/com/masai/SpringBootexceptionhandlingApplication.java
@@ -0,0 +1,15 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootexceptionhandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootexceptionhandlingApplication.class, args);
+	}
+
+}
+
+

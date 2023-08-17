3b9602579fecf43993a041192eb3a269254550fa
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-08-17 23:18:25 +0530

    invalid excepiton

---
+++ SpringBootexceptionhandling/src/main/java/com/masai/InvalidIdException.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+public class InvalidIdException extends RuntimeException{
+	
+	public InvalidIdException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public InvalidIdException(String message) {
+		super(message);
+	}
+
+}

10bda0d38629fa649aad8a234d42c806b966793d
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-08-20 22:14:57 +0530

    main class

---
+++ JavaBasics/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String args[]) {
+//		System.out.println("Hello World.");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter name ");
+		String name = sc.next();
+		System.out.println("Enter age");
+		int age = sc.nextInt();
+		System.out.println("Enter salary");
+		double salary = sc.nextDouble();
+		System.out.println("Enter departmetn");
+		
+		
+	}
+			
+}

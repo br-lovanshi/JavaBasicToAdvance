efbbccef1c3c03bc7e2ea8722e0d0688dfefacfb
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-08-20 21:57:49 +0530

    opps  class

---
+++ Basic Java/Opps.java
@@ -0,0 +1,10 @@
+public class Opps {
+
+	public static void main(String[] args) {
+		
+		Student student1 = new Student("Brajesh",22);
+		String info = student1.toString();
+		 student1.greeting();
+		System.out.println(info);
+}
+}

a818f0670e04d6cc07ca6bdb535aa8aa2bdb2ecb
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-08-22 23:02:02 +0530

    methos or Stream api

--- FunctionalInterface/StreamApi.java
+++ FunctionalInterface/StreamApi.java
@@ -6,6 +6,22 @@ public class StreamApi{
 
     public static void main(String[] args){
 
-        
+        Student st = new Student();
+
+        List<Student> list = new ArrayList<>();
+
+        list.add(new Student(1,"Brajesh",100));
+        list.add(new Student(3,"Vikas",200));
+        list.add(new Student(4,"Laksh",200));
+        list.add(new Student(2,"Shiddharth",300));
+
+        list.stream().forEach(System.out::println);
+
+        list.stream().filter(st -> st.getMakrs() >=200)
+                    .forEach(st -> System.out.println(st.getName));
+
+        List<Student> list1 = list.stream().+
+                                  .filter(st -> st.getMakrs <=100)
+                                  .collect(Collector.toList());            
     }
 }

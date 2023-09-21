2d83123e5d8610a69dc2a4ca6c43ba032dcaf0ef
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-09-21 23:00:38 +0530

    Update Basic.java Immutable class

--- Basic Java/Basic.java
+++ Basic Java/Basic.java
@@ -64,3 +64,22 @@ public class Basic {
 		
 	}
 }
+
+ final class ImmutablePerson {
+    private final String name;
+    private final int age;
+
+    public ImmutablePerson(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+

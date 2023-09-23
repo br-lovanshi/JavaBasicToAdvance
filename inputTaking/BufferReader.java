f5912730304e1bbf95b4f19b09e780af512ac07d
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-09-23 18:20:47 +0530

    reader

---
+++ inputTaking/BufferReader.java
@@ -0,0 +1,24 @@
+package com.inputtacking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BufferReader {
+
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine().trim());
+        String str = bf.readLine();
+        
+        int [] arr = Arrays.stream(bf.readLine().replaceAll(" ", "").split(" ")).
+            mapToInt(Integer::parseInt).toArray();
+   
+        bf.close();
+        
+        System.out.println(n + "\n" + str);
+        
+        }
+}

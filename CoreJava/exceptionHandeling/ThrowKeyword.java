13bcd1b2e80b48dc51a11131afecf5aae3225a7e
br-lovanshi <brajeshlovanshi2001@gmail.com>
2024-01-30 23:02:56 +0530

    throw keyword

--- CoreJava/exceptionHandeling/ThrowKeyword.java
+++ CoreJava/exceptionHandeling/ThrowKeyword.java
@@ -1,2 +1,18 @@
-package exceptionHandeling;public class ThrowKeyword {
+package exceptionHandeling;
+
+public class ThrowKeyword {
+    public static void main(String[] args) {
+        double bankBalance = 5000;
+        double withdrawlAmount = 6000;
+        try{
+               if(bankBalance < withdrawlAmount){
+                   throw new ArithmeticException("Insufficient bank balance.");
+               }else{
+                   bankBalance = bankBalance - withdrawlAmount;
+                   System.out.println("Withdrawl money successfully");
+               }
+            }catch (Exception e){
+            System.out.println(e.getMessage());
+            }
+    }
 }

73cae19f50790936cdcee4beb521444459e4f5e8
br-lovanshi <brajeshlovanshi2001@gmail.com>
2024-03-31 18:45:23 +0530

    car, journey calss

--- CoreJava/dependencyInjection/tightCoupling/Car.java
+++ CoreJava/dependencyInjection/tightCoupling/Car.java
@@ -1,2 +1,14 @@
-package dependencyInjection;public class Car {
+package dependencyInjection.tightCoupling;
+
+import dependencyInjection.looseCoupling.Vehicle;
+
+public class Car implements Vehicle {
+
+    public void trip(){
+        System.out.println("Car trip started.");
+    }
+    @Override
+    public void start(){
+    trip();
+    }
 }
--- CoreJava/dependencyInjection/tightCoupling/Journey.java
+++ CoreJava/dependencyInjection/tightCoupling/Journey.java
@@ -1,2 +1,18 @@
-package dependencyInjection.tightCoupling;public class Journey {
+package dependencyInjection.tightCoupling;
+
+import dependencyInjection.looseCoupling.Vehicle;
+
+public class Journey {
+
+    Vehicle vehicle;
+
+    public void SetV(Vehicle v){
+        this.vehicle = v;
+    }
+
+    public void stardJOurney(){
+        System.out.println("Journey started.");
+        vehicle.start();
+    }
+    
 }

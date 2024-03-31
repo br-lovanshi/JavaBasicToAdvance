3d33fc97b104e8f4b546b0725a154209f625a8d3
br-lovanshi <brajeshlovanshi2001@gmail.com>
2024-03-31 18:44:47 +0530

    bike class

--- CoreJava/dependencyInjection/tightCoupling/Bike.java
+++ CoreJava/dependencyInjection/tightCoupling/Bike.java
@@ -1,2 +1,14 @@
-package dependencyInjection.tightCoupling;public class Bike {
+package dependencyInjection.tightCoupling;
+
+import dependencyInjection.looseCoupling.Vehicle;
+
+public class Bike implements Vehicle {
+
+    public void ride(){
+        System.out.println("Riding on bike");
+    }
+    @Override
+    public void start(){
+        ride();
+    }
 }

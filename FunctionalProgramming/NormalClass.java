916978bbebd22ec19095bd082105d800bf9e1eb6
br-lovanshi <brajeshlovanshi2001@gmail.com>
2024-06-24 22:55:17 +0530

    functional programming

--- FunctionalProgramming/NormalClass.java
+++ FunctionalProgramming/NormalClass.java
@@ -1,2 +1,28 @@
-package FunctionalProgramming;public class NormalClass {
+package functionalProgramming;
+
+abstract class Greet1{
+    public abstract void greet();
+}
+
+class Indians extends Greet1{
+    @Override
+    public void greet(){
+        System.out.println("Namaste!!");
+    }
+    public void ownMethod(){
+        System.out.println("Indian style");
+    }
+
+}
+public class NormalClass {
+    public static void main(String[] args) {
+
+        Greet1 greet1 = new Indians();
+        greet1.greet();
+        // greet1.ownMethod();
+
+        Indians indians = new Indians();
+        indians.greet();
+        indians.ownMethod();
+    }
 }
